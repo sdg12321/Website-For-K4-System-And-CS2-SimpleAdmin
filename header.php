fde73fa57cfbc9fe6da8a08b6a2b15b7ae161d2d
Daniel <151413805+sdg12321@users.noreply.github.com>
2023-11-28 12:02:38 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,13 +1,14 @@
+<!-- Created by SDG - toplay.ro - v1.0.0 -->
 <header>
 <script src="src/script.js" defer></script>
   <nav class="navbar">
-        <div class="menu-toggle" onclick="toggleMenu()">&#9776; Menu</div>
+        <div class="menu-toggle" onclick="toggleMenu()">&#9776; Meniu</div>
         <ul class="nav-list">
-            <li><a href="/">Home</a></li>
-            <li><a href="rank.php">Rank</a></li>
-            <li><a href="stats.php">Stats</a></li>
-            <li><a href="time.php">Time</a></li>
-			<li><a href="/discord.php" target="_blank" rel="noopener">Discord</a></li>
+            <li><a href="/">Acasă</a></li>
+            <li><a href="rank">Rank</a></li>
+            <li><a href="stats">Stats</a></li>
+            <li><a href="ore">Time</a></li>
+			<li><a href="/discord" target="_blank" rel="noopener">Discord</a></li>
 <a href="steam://connect/45.13.151.136:27015" target="_blank" rel="noopener" style="color: #00FF00;">Connect CS2.TOPLAY.RO</a>
         </ul>
     </nav>
