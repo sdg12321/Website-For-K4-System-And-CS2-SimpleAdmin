c229d6f1ef82819c2b14e1c00f01014a6e7effaf
Daniel <151413805+sdg12321@users.noreply.github.com>
2023-12-16 20:32:58 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="src/style.css?v=4.1.0">
-<!-- Created by SDG - toplay.ro v4.1.0 -->
+<link rel="stylesheet" href="src/style.css?v=5.0.0">
+<!-- Created by SDG - toplay.ro v5.0.0 -->
 </head>
 <header>
 <script src="src/script.js" defer></script>
@@ -9,8 +9,9 @@
             <li><a href="/">Home</a></li>
             <li><a href="rank.php">Rank</a></li>
             <li><a href="time.php">Time</a></li>
-			<li><a href="/discord.php" target="_blank" rel="noopener">Discord</a></li>
-<a href="steam://connect/YOURIP:27015" target="_blank" rel="noopener" style="color: #00FF00;">Connect YOURSERVER</a>
+			<li><a href="bans.php">Bans</a></li>
+			<li><a href="mutes.php">Mutes</a></li>
+
         </ul>
     </nav>
 </header>
